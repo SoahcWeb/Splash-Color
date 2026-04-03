79476c37e32670fe4d67e9fe9db613d46616839f
Jp <amonrahtep@gmail.com>
2026-04-03 11:31:28 +0200

    feat: create main layout, routes and pages

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h1>À propos de Splash COLOR</h1>
+@endsection
---
+++ resources/views/services.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+    <h1>Nos Services</h1>
+@endsection
